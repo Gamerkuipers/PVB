d1254534a8ccb8beabaf66ed68b1daebcb12c9e0
gamerkuipers <gamerkuipers@gmail.com>
2023-06-15 16:26:23 +0200

    added database migrations

---
+++ database/migrations/2023_06_15_140812_create_ads_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('brand');
+            $table->text('description');
+            $table->string('price');
+            $table->string('license_plate');
+            $table->integer('build_year');
+            $table->string('body');
+            $table->string('fuel');
+            $table->string('transmission');
+            $table->string('color');
+            $table->string('power');
+            $table->boolean('btw');
+            $table->integer('doors');
+            $table->integer('seating');
+            $table->timestamp('apk_expire_date');
+            $table->string('fuel_usage');
+            $table->string('cylinder_capacity');
+            $table->integer('weight');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ads');
+    }
+};
---
+++ database/migrations/2023_06_15_140946_create_files_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Ad;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('thumbnail')->default(false);
+            $table->text('location');
+            $table->foreignIdFor(Ad::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
