b1b533e5675f497b372e04f11703e30d36945bb3
gamerkuipers <gamerkuipers@gmail.com>
2023-09-05 12:19:31 +0200

    updated seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\WebContent;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
